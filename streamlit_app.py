83102b152182668acbd1794e8e6aede89c353b50
bigjohnsonboi <cruzcastroj@evhs.net>
2026-03-27 16:59:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+)pip install PyQt5 PyQtWebEngine
